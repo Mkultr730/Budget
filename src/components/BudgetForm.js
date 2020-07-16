3bbc473194472f9413001e7baad2e4688170bd13
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-15 21:37:46 -0500

    feat: Error Component

--- src/components/BudgetForm.js
+++ src/components/BudgetForm.js
@@ -1,16 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
 
 const BudgetForm = () => {
+
+    const [amount, setAmount] = useState(0);
+    const [error, setError] = useState(false);
+
+    const setBudget = (e) => {
+        setAmount(parseInt(e.target.value, 10));
+    }
+
+    const submitBudget = (e) => {
+        e.preventDefault();
+
+        if (amount < 1 || isNaN( amount )) {
+            setError(true);
+            return;
+        }
+        setError(false);
+    }
 
     return(
         <Fragment>
             <h2>What is your budget?</h2>
 
-            <form>
+            { error ? <Error message="Invalid Input" /> : null }
+
+            <form
+                onSubmit={submitBudget}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Your budget"
+                    onChange={setBudget}
                 />
                 <input
                     type="submit"
